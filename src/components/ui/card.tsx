ae87c18a5067c313930f5bda07984a189d452156
Lior Pendler <lior.pendler@gmail.com>
2025-10-10 02:28:48 -0700

    Add new shadcn components and import leaflet

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -9,7 +9,7 @@ const Card = React.forwardRef<
 	<div
 		ref={ref}
 		className={cn(
-			'rounded-lg border bg-card text-card-foreground shadow-sm',
+			'rounded-lg bg-background-100 text-text-900 shadow-sm border border-primary-200',
 			className
 		)}
 		{...props}
